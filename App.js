1ae573266fce55c05a793f50244cd4db3ecc9132
echarlotte612 <e.charlotte94@gmail.com>
2022-01-26 21:44:33 +0000

    added react paper searchbar

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.safeArea}>
         <Text>Welcome to LocalPalooza!</Text>
+        <View style={styles.search}>
+          <Searchbar />   
+        </View>
       </SafeAreaView>
       <ExpoStatusBar style="auto" />
     </>
@@ -23,4 +27,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  search: {
+    backgroundColor: '#ffa'
+  }
 });
